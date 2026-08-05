11dfa66038a1be41ac85f36c41a01fb685247456
mohan prakash <mohanprakash9390@gmail.com>
2026-08-05 21:36:49 +0530

    student grade calculator

---
+++ student_grade_calculator.py
@@ -0,0 +1,13 @@
+marks=int(input("Enter your marks: "))
+if marks>=90:
+    print("Grade: A")
+elif marks>=89:
+    print("Grade: B")
+elif marks>=74:
+    print("Grade: C")
+elif marks>=59:
+    print("Grade: D")
+elif marks>=39:
+    print("Grade: F")
+else:
+    print("Invalid Marks")
